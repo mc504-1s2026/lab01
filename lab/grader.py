252d81f6b5bb369344ee4a43c89c2ff1da73f6e7
Vinicius Peixoto <vinicius.peixoto@canonical.com>
2026-03-31 19:50:15 -0300

    lab01: update grader to run only one test suite per boot
    The alloc ktests have side effects (mainly initializing the allocator)
    that leak into the vm_map tests. In order to prevent false positives,
    run each test suite completely separate from each other (e.g. run only
    one test suite per boot).

--- lab/grader.py
+++ lab/grader.py
@@ -5,6 +5,8 @@ import re
 from time import sleep
 from math import ceil
 import sys
+from typing import Optional
+import tempfile
 
 def parse_log(log: str):
     test_results = re.findall(r"^ktest: \[(.*):(.*)\] test result: (SUCCESS|FAILURE)", log, re.M)
@@ -16,55 +18,93 @@ def parse_log(log: str):
         results[suite].append((test, result))
     return results
 
-def get_vm_log(timeout: int = 10, quiet: bool = False):
+def get_vm_log(timeout: int = 10, quiet: bool = False, bootargs: Optional[str] = None):
     subprocess.run(["meson", "setup", "--cross-file=meson-llvm-riscv.ini", "build"], check=True)
     subprocess.run(["meson", "compile", "-C", "build"], check=True)
+
+    tmp = tempfile.NamedTemporaryFile()
 
     qemu_cmd = ["qemu-system-riscv64",
                 "-nographic",
                 "-m", "1G",
                 "-machine", "virt", "-bios", "none",
-                "-serial", "mon:stdio",
+                "-serial", f"file:{tmp.name}",
                 "-kernel", "./build/src/ktest.elf"]
+
+    if bootargs:
+        qemu_cmd += ["-append", bootargs]
+
     if not quiet:
         print(f"running: {" ".join(qemu_cmd)}")
 
-    p = subprocess.Popen(qemu_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    p = subprocess.Popen(qemu_cmd)
     sleep(timeout)
     p.kill()
     p.wait()
-    serial_output = p.stdout.read().decode("utf-8")
-    p.communicate()
+
+    serial_output = None
+    with open(tmp.name) as f:
+        serial_output = f.read()
 
     return serial_output
 
+class TestSuite:
+    def __init__(self, name: str, testcases: list[str], weight: int):
+        self.name = name
+        self.testcases = testcases
+        self.weight = weight
+        self.results = {}
+
+    def parse_results(self, log: str):
+        test_results = re.findall(r"^ktest: \[(.*):(.*)\] test result: (SUCCESS|FAILURE)", log, re.M)
+        for suite, test, result in test_results:
+            if suite != self.name:
+                continue
+            if test not in self.testcases:
+                print(f"[{self.name}] parse_results: unknown test case \"{test}\"; skipping")
+            self.results[test] = result
+
+        for test in self.testcases:
+            if test not in self.results:
+                self.results[test] = "MISSING"
+
 def grade_assignment(timeout: int = 10, quiet: bool = False) -> bool:
-    log = get_vm_log(timeout, quiet)
-    print(log)
-    results = parse_log(log)
-    weights = {
-            "page_unit_tests": 5,
-            "virt_addr_unit_tests": 5,
-            "ppn_unit_tests": 5,
-            "pte_unit_tests": 5,
-            "alloc_tests": 30,
-            "vm_map_tests": 50,
-    }
+    suites = [
+            TestSuite("page_unit_tests",
+                      ["test_is_aligned", "test_ppn_down", "test_ppn_up"],
+                      5),
+            TestSuite("virt_addr_unit_tests",
+                      ["test_va_get_index"], 5),
+            TestSuite("ppn_unit_tests",
+                      ["test_phys_to_ppn", "test_ppn_to_phys", "test_pte_get_ppn", "test_pte_from_ppn"], 5),
+            TestSuite("pte_unit_tests",
+                      ["test_pte_valid", "test_pte_readable", "test_pte_writable", "test_pte_executable", "test_pte_leaf"], 5),
+            TestSuite("alloc_tests",
+                      ["alloc_tests_fail_without_init", "alloc_tests_sanity_checks"], 30),
+            TestSuite("vm_map_tests",
+                      ["vm_map_tests_kernel", "vm_map_tests_basic", "vm_map_tests_refuse_misaligned_va", "vm_map_tests_refuse_misaligned_pa", "vm_map_tests_refuse_malformed_va", "vm_map_tests_refuse_remap"], 50),
+    ]
+
+    for suite in suites:
+        log = get_vm_log(timeout, quiet, bootargs=f"ktest_run={suite.name}")
+        print(log)
+        suite.parse_results(log)
+        print(suite.results)
 
     print("----------[ test results ]----------")
     grade = 0
-    for suite, test_results in results.items():
-        print(f"{suite}:")
+    for suite in suites:
+        print(f"{suite.name}:")
 
         points = 0
-        max_points = len(test_results)
-        for test, res in test_results:
+        max_points = len(suite.results)
+        for test, res in suite.results.items():
             print(f"\t{test}: {res}")
             if res == "SUCCESS":
                 points += 1
 
-        suite_grade = weights[suite] * points / max_points
-        print(f"\tTOTAL: {suite_grade:.2f}/{weights[suite]} points")
+        suite_grade = suite.weight * points / max_points
+        print(f"\tTOTAL: {suite_grade:.2f}/{suite.weight} points")
         grade += suite_grade
         print("------------------------------------")
     print(f"FINAL GRADE: {ceil(grade)}/100 points")
